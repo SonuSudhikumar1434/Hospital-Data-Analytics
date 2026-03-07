904b2fe1381bf37f8573da7ea08b598736bdd0be
sonu <sonusudhikumarjobs@gmail.com>
2026-03-07 15:25:59 +0530

    gold added

---
+++ sql/GOLD/dim_patients.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE TABLE GOLD.dim_patients AS
+SELECT
+      patient_id,
+      birth_date,
+      death_date,
+      life_status,
+      name_prefix,
+      first_name,
+      last_name,
+      name_suffix,
+      gender,
+      age_years,
+      marital_status,
+      maiden_name,
+      race,
+      ethnicity,
+      birth_place,
+      address,
+      city,
+      state,
+      county,
+      zip,
+      latitude,
+      longitude
+   
+FROM SILVER.stg_patients;
